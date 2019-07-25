eb2a823cb37605f8dc5071aeb7cd5019f98cd39e
Alexey <50285726+Alex-Kristo@users.noreply.github.com>
2019-07-25 19:41:59 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,5 +1,5 @@
 class TestMainPage(object):
-    def test_basket_presence(self, driver):
+    def test_add_to_basket_button_presence(self, driver):
         driver.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
         basket_selector = '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]'
         driver.find_element_by_xpath(basket_selector)
